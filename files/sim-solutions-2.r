857ba2b1abe3321346de826d754408328c981fd7
aoliveram <anibal.olivera.m@gmail.com>
2025-06-10 19:21:44 -0400

    adding sim-solutions

--- files/sim-solutions-2.r
+++ files/sim-solutions-2.r
@@ -3,57 +3,135 @@
 # Loading the package
 library(netdiffuseR)
 
-# Loading the data
-kfamily_21 <- subset(kfamily, village == 21)
-
-# Converting the survey data to a diffnet object
-kfamily_diffnet_21 <- survey_to_diffnet(
-  dat      = kfamily_21,
-  idvar    = "id",
-  netvars  = c(
-    "net11", "net12", "net13", "net14", "net15", 
-    "net21", "net22", "net23", "net24", "net25",
-    "net31", "net32", "net33", "net34", "net35"), 
-  toavar   = "toa",
-  groupvar = "village"
-)
-
 # First, some common parameters:
 adoption <- function(x) cumulative_adopt_count(x)[2,nslices(x)]
-nsim     <- 500
+nsim     <- 1e3L
 ncores   <- 4
 
-# Korean Family Planning - village 21 network ----------------------------------
-
-# random seeds
-sims_rand <- rdiffnet_multiple(
-  seed.graph = kfamily_diffnet_21,
+# Small-world networks ---------------------------------------------------------
+ans_sw_rand <- rdiffnet_multiple(
   R          = nsim, 
   statistic  = adoption,
   ncpus      = ncores,
+  n          = 100,
+  t          = 10,
+  rewire     = FALSE,
   stop.no.diff = FALSE,
+  seed.graph = "small-world",
   seed.nodes = "random"
 )
-# central seeds
-sims_cent <- rdiffnet_multiple(
-  seed.graph = kfamily_diffnet_21,
+
+ans_sw_cent <- rdiffnet_multiple(
   R          = nsim, 
   statistic  = adoption,
   ncpus      = ncores,
+  n          = 100,
+  t          = 10,
+  rewire     = FALSE,
   stop.no.diff = FALSE,
+  seed.graph = "small-world",
   seed.nodes = "central"
 )
-# marginal seeds
-sims_marg <- rdiffnet_multiple(
-  seed.graph = kfamily_diffnet_21,
+
+ans_sw_marg <- rdiffnet_multiple(
   R          = nsim, 
   statistic  = adoption,
   ncpus      = ncores,
+  n          = 100,
+  t          = 10,
+  rewire     = FALSE,
   stop.no.diff = FALSE,
+  seed.graph = "small-world",
   seed.nodes = "marginal"
 )
-# Plotting the results
-boxplot(
-  cbind(Random = sims_rand, Central = sims_cent, Marginal = sims_marg),
-  main = "Korean Family Planning - village 21 network"
+
+# Scale-free- networks ---------------------------------------------------------
+ans_sf_rand <- rdiffnet_multiple(
+  R          = nsim, 
+  statistic  = adoption,
+  ncpus      = ncores,
+  n          = 100,
+  t          = 10,
+  rewire     = FALSE,
+  stop.no.diff = FALSE,
+  seed.graph = "scale-free",
+  seed.nodes = "random"
 )
+
+ans_sf_cent <- rdiffnet_multiple(
+  R          = nsim, 
+  statistic  = adoption,
+  ncpus      = ncores,
+  n          = 100,
+  t          = 10,
+  rewire     = FALSE,
+  stop.no.diff = FALSE,
+  seed.graph = "scale-free",
+  seed.nodes = "central"
+)
+
+ans_sf_marg <- rdiffnet_multiple(
+  R          = nsim, 
+  statistic  = adoption,
+  ncpus      = ncores,
+  n          = 100,
+  t          = 10,
+  rewire     = FALSE,
+  stop.no.diff = FALSE,
+  seed.graph = "scale-free",
+  seed.nodes = "marginal"
+)
+
+# Bernoulli graphs -------------------------------------------------------------
+
+ans_er_rand <- rdiffnet_multiple(
+  R          = nsim, 
+  statistic  = adoption,
+  ncpus      = ncores,
+  n          = 100,
+  t          = 10,
+  rewire     = FALSE,
+  stop.no.diff = FALSE,
+  seed.graph = "bernoulli",
+  seed.nodes = "random"
+)
+
+ans_er_cent <- rdiffnet_multiple(
+  R          = nsim, 
+  statistic  = adoption,
+  ncpus      = ncores,
+  n          = 100,
+  t          = 10,
+  rewire     = FALSE,
+  stop.no.diff = FALSE,
+  seed.graph = "bernoulli",
+  seed.nodes = "central"
+)
+
+ans_er_marg <- rdiffnet_multiple(
+  R          = nsim, 
+  statistic  = adoption,
+  ncpus      = ncores,
+  n          = 100,
+  t          = 10,
+  rewire     = FALSE,
+  stop.no.diff = FALSE,
+  seed.graph = "bernoulli",
+  seed.nodes = "marginal"
+)
+
+
+op <- par(mfrow = c(2, 2))
+boxplot(
+  cbind(Central = ans_sw_cent, Marginal = ans_sw_marg, Random = ans_sw_rand),
+  main = "Small-world"
+)
+boxplot(
+  cbind(Central = ans_sf_cent, Marginal = ans_sf_marg, Random = ans_sf_rand),
+  main = "Scale-free"
+)
+boxplot(
+  cbind(Central = ans_er_cent, Marginal = ans_er_marg, Random = ans_er_rand),
+  main = "Bernoulli"
+)
+par(op)
